dc2ce01a121c2dba9dfa3d356bc6f26d90eeba63
Jaysama137 <liubang2019@gmail.com>
2023-09-08 16:37:20 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# polyticket-master
-a homework about buying tickets online
+# AutoTicketForPoly
+
+#### 介绍
+保利票务自动抢票脚本
+
+#### 软件架构
+基于python3
+selenium
+chromedriver
+
+
+#### 安装教程
+
+1.  选择git clone 或者直接下载压缩文件到本地
+2.  下载python3.7  https://blog.csdn.net/weixin_40844416/article/details/80889165
+3.  python3.7安装好之后，打开cmd终端输入pip install selenium，等待安装完毕
+![输入图片说明](https://images.gitee.com/uploads/images/2021/0802/095017_ae037ff4_9510992.png "屏幕截图.png")
+4.  如果没有chrome，下载chrome。chrome的版本信息在浏览器的“帮助”菜单中能找到
+5.  下载chrome **对应版本** 的chromedriver，网址：https://npm.taobao.org/mirrors/chromedriver/
+6.  生成一个chrome浏览器的快捷方式到此文件夹
+![输入图片说明](https://images.gitee.com/uploads/images/2021/0801/120050_3dc87c05_9510992.png "屏幕截图.png")
+7.  右键chrome快捷方式，选择“属性”，设置“目标”  
+举例：**"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="E:\Data\02_Code\07_Ticket_Order\Automatic_ticket_purchase-master\AutomationProfile"** 
+这个地方填写的是debug模式启动chrome命令，这样才能让webdriver通过port口获取浏览器页面，需要在本文件夹下面创建一个AutomationProfile，来存放临时文件
+
+命令的格式是  chrome原文件的路径  --remote-debugging-port=9222 --user-data-dir="临时文件存放路径"
+
+然后设置起始位置为本文件夹
+
+举例：E:\Data\02_Code\07_Ticket_Order\Automatic_ticket_purchase-master  这里设置了chrome的root路径
+![输入图片说明](https://images.gitee.com/uploads/images/2021/0801/120309_0ec25c66_9510992.png "屏幕截图.png")
+
+这两步是为了让webdriver可以获取到当前的浏览器页面，这个页面是登陆之后的页面，所以不存在登陆的验证问题
+
+
+#### 使用说明
+
+1.  进入文件夹，打开chrome快捷方式
+2.  用txt文本软件打开ticket_order.py编辑以下两行
+![输入图片说明](https://images.gitee.com/uploads/images/2021/0802/095822_8bb45f2e_9510992.png "屏幕截图.png")
+    关闭ticket_order.py
+3.  打开cmd终端，使用命令 python ticket_order.py，开始运行
+![输入图片说明](https://images.gitee.com/uploads/images/2021/0802/095943_222b7343_9510992.png "屏幕截图.png")
+ **注意保持浏览器页面在屏幕前面** （眼睛能看到的地方，这个软件有些鼠标自动点击的地方需要浏览器在最前面）
+![输入图片说明](https://images.gitee.com/uploads/images/2021/0802/100116_e03c24a1_9510992.png "屏幕截图.png")
+
+4.  等待打印出来 “抢票成功，请尽快支付” 时表示抢票成功
+
+#### 参与贡献
+
+1.  Fork 本仓库
+2.  新建 Feat_xxx 分支
+3.  提交代码
+4.  新建 Pull Request
+
+
+#### 特技
+
+1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
+2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
+3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
+4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
+5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
+6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
---
+++ ticket_order.py
@@ -0,0 +1,193 @@
+from os import name
+import select
+from xml.sax.xmlreader import Locator
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import re
+import time
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+#测试使用chrome版本：116.0.5845.180
+
+#抢票目标页
+target_url="https://whqtdjy.polyt.cn/#/detail?productId=5896100"
+
+class Concert(object):
+    def __init__(self):
+        self.status = 0
+        self.login_method = 0
+        self.date = 1  #选择哪一天，按数字排列，比如2021-01-01，2021-01-02，2021-01-03可选，想选01-01填1，想选01-02填2
+        self.price= 2  #买哪一档票，从贵到便宜，1，2，3
+        self.total_wait_time = 5   #WebDriverWait总等待时间
+        self.refresh_wait_time = 0.1 #WebDriverWait刷新动作的时间
+        self.name = "张三"
+        self.phonenum = "11451411451"
+        chrome_options = Options()
+        chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")  #控制当前chrome页面
+        chrome_driver = "D:/auto-ticket-for-poly-master/chromedriver.exe" # 指定自己的chromedriver路径
+        self.driver = webdriver.Chrome(chrome_driver, options=chrome_options)
+        print("初始化成功，载入页面中")
+    
+    def isClassPresent(self, item, name, ret=False):
+        try:
+            result = item.find_element_by_class_name(name)
+            if ret:
+                return result
+            else:
+                return True
+        except:
+            return False
+
+
+    def prepare(self):
+        self.driver.get(target_url)
+        self.driver.refresh()
+        start = False
+        locator = (By.XPATH, "//*[@id=\"app\"]/div/section/div[2]/div/div/div[2]/div/div[1]/div[2]/div[2]/div/div[1]/div[2]")
+        while not start:
+            try:
+                WebDriverWait(self.driver, 100,0.1).until(EC.text_to_be_present_in_element(locator,'售票中'))
+                print("获取页面成功")
+                break
+            except Exception as e:
+                print("未开始")
+
+    def choose_ticket(self):
+        choiceTime_xpath = "//*[@id=\"app\"]/div/section/div[2]/div/div/div[2]/div/div[1]/div[2]/div[2]/div/div[2]/div[2]" #售票时间
+        choicePrice_xpath = "//*[@id=\"app\"]/div/section/div[2]/div/div/div[2]/div/div[1]/div[2]/div[2]/div/div[3]/div[2]" #售票价位
+        checkbut_xpath  = "//*[@id=\"app\"]/div/section/div[2]/div/div/div[2]/div/div[1]/div[2]/div[2]/div/div[4]/div[2]/button"#选座购买按钮
+
+        while self.driver.current_url.find('selectSeat') == -1:           #如果跳转到了订单结算界面就算这步成功了，否则继续执行此步
+            #选择日期
+            print("="*30)
+            print("###开始进行日期选择###") 
+
+            #选择时间
+            choiceTime = WebDriverWait(self.driver, self.total_wait_time, self.refresh_wait_time).until(
+                    EC.presence_of_element_located((By.XPATH, choiceTime_xpath)))
+            choiceTime_list = choiceTime.find_elements_by_tag_name('span')  
+
+            if self.date>len(choiceTime_list):
+                actualDate = choiceTime_list[0]
+                print("想选的日期不存在，自动选择" + actualDate)
+                choiceTime_list[0].click()
+            else:
+                choiceTime_list[self.date-1].click()
+            print("日期选择中")
+
+
+            """
+            #选择价位
+            print("="*30)
+            print("###开始进行价位选择###")
+            
+            choicePrice = WebDriverWait(self.driver, self.total_wait_time, self.refresh_wait_time).until(
+                    EC.presence_of_element_located((By.XPATH, choicePrice_xpath)))
+            choicePrice_list = choicePrice.find_elements_by_tag_name('span')  
+
+            if self.price>len(choicePrice_list):
+                actualPrice = choicePrice_list[0]
+                print("想选的价位不存在，自动选择" + actualPrice)
+                choicePrice_list[0].click()
+            else:
+                choicePrice_list[self.date-1].click()
+            
+            print("价位选择中")
+            """
+
+            #按下选座购票按钮
+            checkbut = self.driver.find_element_by_xpath(checkbut_xpath)
+            checkbut.click()
+
+            time.sleep(self.refresh_wait_time)
+
+        print("###日期价位选择成功###")
+
+    def seatSelectbyhand(self):
+        print("="*30)
+        print("###开始进行座位选择###")
+        fullscreenbut_xpath = "//*[@id=\"seatContent\"]/div/div[1]/div[2]/div/div[2]"
+        confirmbut_xpath = "//*[@id=\"seatContent\"]/div/div[3]/div/div[3]/div[1]/div/div/section/div[2]/button"
+
+        fullscreenbut = WebDriverWait(self.driver, self.total_wait_time, self.refresh_wait_time).until(
+                EC.presence_of_element_located((By.XPATH, fullscreenbut_xpath)))
+        fullscreenbut.click()
+
+        print("正在等待手动选择")
+
+        while self.driver.current_url.find('orderDetail') == -1:
+            print("正在等待跳转支付页面")
+            
+
+            #等待出现结账按钮
+            try:
+                # 等待结账按钮出现并且可以点击
+                element = WebDriverWait(self.driver, self.total_wait_time, self.refresh_wait_time).until(
+                    EC.element_to_be_clickable((By.XPATH, confirmbut_xpath))
+                )
+                
+                element.click()
+                
+                print("###手动座位选择成功###")
+
+                time.sleep(self.refresh_wait_time)
+                
+            except Exception as e:
+                print("手动座位选择失败:", str(e))
+
+            
+        print("###座位选择成功###")
+            
+    def checkout(self):
+        print("="*30)
+        print("###开始填写支付信息###")
+
+        name_input_xpath = "//*[@id=\"pane-self\"]/div/div[2]/form/div[1]/div/div/input"
+        phontnum_input_xpath = "//*[@id=\"pane-self\"]/div/div[2]/form/div[2]/div/div/input"
+        payway_xpath = "//*[@id=\"app\"]/div/section/div[2]/div/div/div/div[3]/div[2]/div[1]" #第一项，微信支付
+        agreement_xpath = "//*[@id=\"app\"]/div/section/div[2]/div/div/div/div[5]/div[2]/div[1]/div/label/span[1]/span"
+        checkoutbut_xpath = "//*[@id=\"app\"]/div/section/div[2]/div/div/div/div[5]/div[2]/div[2]/button"
+
+        name_input = WebDriverWait(self.driver, self.total_wait_time, self.refresh_wait_time).until(
+                    EC.presence_of_element_located((By.XPATH, name_input_xpath)))
+        name_input.clear()
+        name_input.send_keys(self.name)
+        print("###姓名填写成功###")
+
+        phontnum_input = self.driver.find_element_by_xpath(phontnum_input_xpath)
+        phontnum_input.clear()
+        phontnum_input.send_keys(self.phonenum)
+        print("###手机号填写成功###")
+
+        payway = self.driver.find_element_by_xpath(payway_xpath)
+        payway.click()
+        print("###支付方式选择成功###")
+
+        agreement = self.driver.find_element_by_xpath(agreement_xpath)
+        agreement.click()
+        print("###同意协议成功###")
+
+        checkoutbut = self.driver.find_element_by_xpath(checkoutbut_xpath)
+        checkoutbut.click()
+        print("###订单提交成功###")
+
+
+if __name__ == '__main__':
+    try:
+        con = Concert()             #具体如果填写请查看类中的初始化函数
+        con.prepare()
+        con.choose_ticket()
+        con.seatSelectbyhand()
+        con.checkout()
+
+    except Exception as e:
+        print(e)
+        con.finish()
+
+
+
+
